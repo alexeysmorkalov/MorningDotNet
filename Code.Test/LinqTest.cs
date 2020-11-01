550ef36a0c33a3d3cc6c778e7f85473dbbae1269
Alexey Smorkalov <magen-ta@yandex.ru>
2020-11-01 07:35:44 +0300

    Linq_Aggregate_Test

---
+++ Code.Test/LinqTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Code.Test
+{
+    [TestFixture]
+    public class LinqTest
+    {
+        [Test]
+        [TestCase("*123", "1*2*3")]
+        [TestCase("", "")]
+        [TestCase("*", "")]
+        [TestCase("*1", "1")]
+        [TestCase("**", "*")]
+        public void Linq_Aggregate_Test(string input, string expected) {
+            // Arrange
+            string result = "";
+            char[] data = input.ToCharArray();
+            // Act
+            result = data.Skip(1).Aggregate("", (list, next) => string.IsNullOrEmpty(list) ? Convert.ToString(next) : list + data[0] + Convert.ToString(next));
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
